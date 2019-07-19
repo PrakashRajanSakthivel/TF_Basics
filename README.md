77640acb427932593cbbe0b60b5965b5b46bce4f
PrakashRajanSakthivel <45005244+PrakashRajanSakthivel@users.noreply.github.com>
2019-07-19 17:39:28 +0530

    Added basic test code 1

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # TF_Basics
-Tensor Flow Basics using Python
+Tensor Flow Basics using Python.
+
+Machine - windows 10 <br />
+Python version - 3.7.0 <br />
+TF Version - 1.14.0
+
+
+Issues faced: After installing tensorflow, while running the code was getting the error 
+"Your CPU supports instructions that this TensorFlow binary was not compiled to use: AVX AVX2". 
+Solution:  https://stackoverflow.com/questions/47068709/your-cpu-supports-instructions-that-this-tensorflow-binary-was-not-compiled-to-u -- solution by Z.Wei worked
+
+ 
+
+
--- Simple_Math_Operation.py
+++ Simple_Math_Operation.py
@@ -6,9 +6,9 @@ c = tf.constant(12, name="var_c")
 d = tf.constant(3, name="var_d")
 
 mul = tf.multiply(a, b, name="multiply_operation")
-# Deprecated in favor of operator or tf.math.divide.
+# Error: Deprecated in favor of operator or tf.math.divide.
 # div = tf.div(c, d, name="divide_operation")
-# Tensors in list passed to 'inputs' of 'AddN' Op have types [int32, float64] that don't all match.
+# Error: Tensors in list passed to 'inputs' of 'AddN' Op have types [int32, float64] that don't all match.
 # typecast the output to int
 div = tf.cast(tf.math.divide(c, d, name="divide_operation"), tf.int32)
 
@@ -22,10 +22,9 @@ with tf.Session() as session:
 
 # simple_math_operation folder will be created
 # to run that open command prompt -> python tensorboard --logdir="foldername"
-# tensorboard --logdir="C:\Users\adminuser\PycharmProjects\pythontf\simple_math_operation"
+# tensorboard --logdir="entire\folder\path"
 # url will be show -> load that in the browser
 
 # if facing issue in loading the url
-# tensorboard --logdir="C:\Users\adminuser\PycharmProjects\pythontf\simple_math_operation" --host localhost
-# --port 8085(anyport)
+# tensorboard --logdir="entire\folder\path" --host localhost --port 8085(anyport)
 
